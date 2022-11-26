2bb5326b20128c0f6b9a8b751fc53be12fcffe20
daimon <jaikan@hotmail.it>
2022-11-26 07:10:16 +0100

    add plugin

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import setup_translate
+
+
+pkg='Extensions.InfoBarWeather'
+setup(name='enigma2-plugin-extensions-infobarweather',
+		version='0.10',
+		author='scriptmelvin',
+		author_email='',
+		package_dir={pkg: 'plugin'},
+		packages=[pkg],
+		package_data={pkg: ['*.png', '*/*.png', '*/*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+		description='Show current weather in infobar',
+		license='GPLv2',
+		cmdclass=setup_translate.cmdclass
+	)
